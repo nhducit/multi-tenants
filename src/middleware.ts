66e4fb540594be6fec1b409369feb1006236a9d1
Duc Nguyen <duc.nguyen@ascenda.com>
2025-02-28 18:08:11 +0700

    Update Next.js and dependencies to version 15.2.0

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
 import Negotiator from "negotiator";
 import { i18n } from "./i18n-config";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 function getLocale(request: NextRequest): string | undefined {
 	// Negotiator expects plain object so we need to transform headers
 	const negotiatorHeaders: Record<string, string> = {};
-	request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+	request.headers.forEach((value, key) => {
+		negotiatorHeaders[key] = value;
+	});
 
 	// @ts-ignore locales are readonly
 	const locales: string[] = i18n.locales;
 
 	// Use negotiator and intl-localematcher to get best locale
-	let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
+	const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
 		locales,
 	);
 
@@ -39,8 +42,8 @@ export default async function middleware(request: NextRequest) {
 
 	// Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
 	const hostname = request.headers
-		.get("host")!
-		.replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+		.get("host")
+		?.replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
 	// Get the pathname of the request (e.g. /, /about, /blog/first-post)
 	const pathname = url.pathname;
